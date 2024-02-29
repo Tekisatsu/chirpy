0ad82a0c15ff1a309328d7fd4f439adc3e7e30ef
Tekisatsu <ale.hartikainen@gmail.com>
2024-02-29 18:18:43 +0200

    server returning 404 to everything

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
+	
+func NewServeMux() *http.ServeMux {
+	return http.NewServeMux()
+}
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 func main () {
-	fmt.Println("chirp chirp")
+	mux := NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+		})
+	corsMux := middlewareCors(mux)
+	srv := &http.Server {
+		Addr: "localhost:8080",
+		Handler: corsMux,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
